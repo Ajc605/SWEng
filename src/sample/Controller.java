180fa54d831a2722da92c187f0af9ccc95c3ee35
Amy Jo <turneramyjo@gmail.com>
2018-03-15 18:10:47 +0000

    The images are added into git

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,13 +3,19 @@ package sample;
 
 //import java.awt.event.ActionEvent;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.image.*;
 
 import java.io.IOException;
+
+import static javafx.scene.image.ImageView.*;
 
 public class Controller {
 
@@ -21,7 +27,7 @@ public class Controller {
         //Making a new scene with new layout
         Scene LoginScene = new Scene(ChangeScene);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(LoginScene);
         window.show();
     }
@@ -31,9 +37,9 @@ public class Controller {
         //Getting the layout from file
         Parent SettingLayout = FXMLLoader.load(getClass().getResource("settingmenu.fxml"));
         //Making a new scene with new layout
-        Scene SettingScene  = new Scene(SettingLayout);
+        Scene SettingScene = new Scene(SettingLayout);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(SettingScene);
         window.show();
     }
@@ -43,9 +49,9 @@ public class Controller {
         //Getting the layout from file
         Parent MainLayout = FXMLLoader.load(getClass().getResource("sample.fxml"));
         //Making a new scene with new layout
-        Scene mainScene  = new Scene(MainLayout);
+        Scene mainScene = new Scene(MainLayout);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(mainScene);
         window.show();
     }
@@ -54,9 +60,9 @@ public class Controller {
         //Getting the layout from file
         Parent welcomePictureLayout = FXMLLoader.load(getClass().getResource("welcomePicture.fxml"));
         //Making a new scene with new layout
-        Scene welcomePictureScene  = new Scene(welcomePictureLayout);
+        Scene welcomePictureScene = new Scene(welcomePictureLayout);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(welcomePictureScene);
         window.show();
     }
@@ -66,9 +72,9 @@ public class Controller {
         //Getting the layout from file
         Parent mapLayout = FXMLLoader.load(getClass().getResource("map.fxml"));
         //Making a new scene with new layout
-        Scene mapScene  = new Scene(mapLayout);
+        Scene mapScene = new Scene(mapLayout);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(mapScene);
         window.show();
     }
@@ -78,14 +84,13 @@ public class Controller {
         //Getting the layout from file
         Parent area1Layout = FXMLLoader.load(getClass().getResource("area1.fxml"));
         //Making a new scene with new layout
-        Scene area1Scene  = new Scene(area1Layout);
+        Scene area1Scene = new Scene(area1Layout);
+        magCursor(area1Scene);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(area1Scene);
         window.show();
     }
-
-
 
 
     public void toArea2(ActionEvent event) throws IOException {
@@ -93,9 +98,10 @@ public class Controller {
         //Getting the layout from file
         Parent area2Layout = FXMLLoader.load(getClass().getResource("area2.fxml"));
         //Making a new scene with new layout
-        Scene area2Scene  = new Scene(area2Layout);
+        Scene area2Scene = new Scene(area2Layout);
+        magCursor(area2Scene);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(area2Scene);
         window.show();
     }
@@ -105,9 +111,10 @@ public class Controller {
         //Getting the layout from file
         Parent area3Layout = FXMLLoader.load(getClass().getResource("area3.fxml"));
         //Making a new scene with new layout
-        Scene area3Scene  = new Scene(area3Layout);
+        Scene area3Scene = new Scene(area3Layout);
+        magCursor(area3Scene);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(area3Scene);
         window.show();
     }
@@ -117,9 +124,10 @@ public class Controller {
         //Getting the layout from file
         Parent area4Layout = FXMLLoader.load(getClass().getResource("area4.fxml"));
         //Making a new scene with new layout
-        Scene area4Scene  = new Scene(area4Layout);
+        Scene area4Scene = new Scene(area4Layout);
+        magCursor(area4Scene);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(area4Scene);
         window.show();
     }
@@ -129,11 +137,33 @@ public class Controller {
         //Getting the layout from file
         Parent area1aLayout = FXMLLoader.load(getClass().getResource("area1a.fxml"));
         //Making a new scene with new layout
-        Scene area1aScene  = new Scene(area1aLayout);
+        Scene area1aScene = new Scene(area1aLayout);
         //Used to get the current window
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(area1aScene);
         window.show();
     }
+
+
+    public void magCursor(Scene x) {
+        Image image = new Image(getClass().getResource("MediaSweng/cursor.png").toExternalForm(),
+                20, 20, false, true);
+        x.setCursor(new ImageCursor(image, 1, 1));
+    }
+
+   public void excCursor(MouseEvent evt) {
+
+        Image image = new Image(getClass().getResource("MediaSweng/exclamation.png").toExternalForm(),
+               20,20,false,true);
+        ((Node)evt.getSource()).getScene().setCursor(new ImageCursor(image, 1, 1));
+    }
+
+    public void mag2Cursor(MouseEvent evt) {
+        magCursor(((Node)evt.getSource()).getScene());
+    }
+
+//    public void unlockAnimation(Stage primaryStage) throws Exception {
+//        ImageView.setStyle("-fx-image: url(\"MediaSweng/unlock.png\");");
+//    }
 
 }
